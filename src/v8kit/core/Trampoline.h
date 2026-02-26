bef5b654e99a7d7f3772575b63056b22e418703e
engsr6982 <pwxg286704@163.com>
2026-02-26 16:29:54 +0800

    refactor(core): restructure override macros for better modularity

--- src/v8kit/core/Trampoline.h
+++ src/v8kit/core/Trampoline.h
@@ -60,7 +60,7 @@ struct OverrideReturner<void> {
 
 } // namespace v8kit
 
-#define V8KIT_OVERRIDE(RETURN_TYPE, BASE_CLASS, SCRIPTMETHOD, METHOD, ...)                                             \
+#define V8KIT_OVERRIDE_IMPL(RETURN_TYPE, BASE_CLASS, SCRIPTMETHOD, METHOD, ...)                                        \
     {                                                                                                                  \
         /* 每个成员函数内的 static 变量都有唯一的内存地址 */                                                           \
         /* 利用这个地址作为该方法的 methodId */                                                                        \
@@ -75,28 +75,23 @@ struct OverrideReturner<void> {
                 return ::v8kit::binding::OverrideReturner<RETURN_TYPE>::convert(value);                                \
             }                                                                                                          \
         }                                                                                                              \
-    }                                                                                                                  \
+    }
+
+
+#define V8KIT_OVERRIDE(RETURN_TYPE, BASE_CLASS, SCRIPTMETHOD, METHOD, ...)                                             \
+    V8KIT_OVERRIDE_IMPL(RETURN_TYPE, BASE_CLASS, SCRIPTMETHOD, METHOD, __VA_ARGS__)                                    \
     /* 1. 如果是递归调用 (super.onLoad) */                                                                             \
     /* 2. 如果 JS 没重写 (nativeFunctionTag_ 命中) */                                                                  \
     /* 则执行 C++ 默认实现 */                                                                                          \
     return BASE_CLASS::METHOD(__VA_ARGS__)
 
 // virtual void xxx() = 0;
-#define V8KIT_OVERRIDE_PURE(RETURN_TYPE, SCRIPTMETHOD, ...)                                                            \
-    {                                                                                                                  \
-        static const char        __v8kit_method_id = 0;                                                                \
-        ::v8kit::TrampolineGuard __guard{this, &__v8kit_method_id};                                                    \
-        if (!__guard.is_recursive) {                                                                                   \
-            auto _maybe_override_ = this->getOverride(::v8kit::String::newString(SCRIPTMETHOD));                       \
-            if (_maybe_override_.isFunction()) {                                                                       \
-                auto _override_fn_ = _maybe_override_.asFunction();                                                    \
-                auto value =                                                                                           \
-                    ::v8kit::binding::call(_override_fn_, this->getThis().asObject() __VA_OPT__(, ) __VA_ARGS__);      \
-                return ::v8kit::binding::OverrideReturner<RETURN_TYPE>::convert(value);                                \
-            }                                                                                                          \
-        }                                                                                                              \
-    }                                                                                                                  \
+#define V8KIT_OVERRIDE_PURE(RETURN_TYPE, BASE_CLASS, SCRIPTMETHOD, METHOD, ...)                                        \
+    V8KIT_OVERRIDE_IMPL(RETURN_TYPE, BASE_CLASS, SCRIPTMETHOD, METHOD, __VA_ARGS__)                                    \
     /* 既然是纯虚函数，JS 侧调用 super.method() 应该什么都不做或者抛出异常 */                                          \
     if constexpr (!std::is_void_v<RETURN_TYPE>) {                                                                      \
-        throw ::v8kit::Exception("Pure virtual method called without JS override", Exception::Type::TypeError);        \
+        throw ::v8kit::Exception(                                                                                      \
+            "Tried to call pure virtual function: " #BASE_CLASS "::" #METHOD "(JS: "#SCRIPTMETHOD ")",                \
+            ::v8kit::Exception::Type::TypeError                                                                        \
+        );                                                                                                             \
     }
