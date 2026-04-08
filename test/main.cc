e612199aa3cdf096da2a710543311f7dbd0d22c1
engsr6982 <pwxg286704@163.com>
2026-04-09 00:24:24 +0800

    refactor(test): guard v8 initialization with JSPP_BACKEND_V8

--- test/main.cc
+++ test/main.cc
@@ -2,6 +2,7 @@
 
 #include <catch2/catch_session.hpp>
 
+#ifdef JSPP_BACKEND_V8
 JSPP_WARNING_GUARD_BEGIN
 #include <libplatform/libplatform-export.h>
 #include <libplatform/libplatform.h>
@@ -9,15 +10,20 @@ JSPP_WARNING_GUARD_BEGIN
 #include <v8-initialization.h>
 #include <v8-platform.h>
 JSPP_WARNING_GUARD_END
+#endif
 
 int main(int argc, char* argv[]) {
+#ifdef JSPP_BACKEND_V8
     auto plat = v8::platform::NewDefaultPlatform(1);
     v8::V8::InitializePlatform(plat.get());
     v8::V8::Initialize();
+#endif
 
     int code = Catch::Session().run(argc, argv);
 
+#ifdef JSPP_BACKEND_V8
     v8::V8::Dispose();
     v8::V8::DisposePlatform();
+#endif
     return code;
 }
