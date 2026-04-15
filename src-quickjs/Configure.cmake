29ef9cb6d604a0b22d4c68975b62a71ae6862387
engsr6982 <pwxg286704@163.com>
2026-04-15 20:21:52 +0800

    build(quickjs): move QuickJS-ng config to test CMakeLists

--- src-quickjs/Configure.cmake
+++ src-quickjs/Configure.cmake
@@ -7,16 +7,6 @@ file(GLOB_RECURSE QUICKJS_SRC "src-quickjs/*.cc")
 target_sources(jspp PRIVATE ${QUICKJS_SRC})
 target_include_directories(jspp PUBLIC "src-quickjs")
 
-set(BUILD_EXECUTABLES OFF CACHE BOOL "" FORCE)
-set(BUILD_TESTS OFF CACHE BOOL "" FORCE)
-
-if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/deps/quickjs-ng/CMakeLists.txt")
-    add_subdirectory(deps/quickjs-ng EXCLUDE_FROM_ALL)
-    target_link_libraries(jspp PUBLIC qjs)
-else()
-    message(FATAL_ERROR "QuickJS-ng source not found in deps/")
-endif()
-
 if(UNIX AND NOT APPLE)
     target_link_libraries(jspp PUBLIC dl m)
 endif()
