7ed577a4b7e0aa37cbf1bd23c86f148577082bae
engsr6982 <pwxg286704@163.com>
2026-05-12 23:40:51 +0800

    refactor: move JSPP_EXTERNAL_INC and JSPP_EXTERNAL_LIB to root CMakeLists.txt

--- src-v8/Configure.cmake
+++ src-v8/Configure.cmake
@@ -6,11 +6,3 @@ target_compile_definitions(jspp PUBLIC JSPP_BACKEND_V8)
 file(GLOB_RECURSE V8_SRC "src-v8/*.cc")
 target_sources(jspp PRIVATE ${V8_SRC})
 target_include_directories(jspp PUBLIC "src-v8")
-
-# external include
-if(JSPP_EXTERNAL_INC)
-    target_include_directories(jspp PUBLIC ${JSPP_EXTERNAL_INC})
-endif()
-if(JSPP_EXTERNAL_LIB)
-    target_link_libraries(jspp PUBLIC ${JSPP_EXTERNAL_LIB})
-endif()
